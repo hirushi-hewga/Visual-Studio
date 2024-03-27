be7bbecd505b1f89902cd6c135e6e632c5780b0c
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-03-27 08:29:16 -0700

    HW (Sort)

--- CPlusPlus/2024-03-18 (Lenears search, Sort)/HW.cpp
+++ CPlusPlus/2024-03-18 (Lenears search, Sort)/HW.cpp
@@ -3,12 +3,32 @@
 using namespace std;
 int main()
 {
+	/*
 	srand(time(NULL));
 	const int size = 10;
 	int arr[size]{};
 	InitArray(arr, size);
-	cout << *arr;
+	ShowArray(arr, size);
+	int choice;
+	cout << "1 - sortuvannya za zrostannyam" << endl;
+	cout << "2 - sortuvannya za spadannyam" << endl;
+	cout << "Enter your choice: " << endl;
+	cin >> choice;
+	InsertSort(arr, size, choice - 1);
+	ShowArray(arr, size);
+	*/
 
+
+
+
+
+	srand(time(NULL));
+	const int size = 10;
+	int arr[size]{};
+	InitArray2(arr, size);
+	ShowArray(arr, size);
+	int* sortedArray = Function(arr, size);
+	ShowArray(sortedArray, size);
 
 
 
@@ -33,6 +53,15 @@ int main()
 	*/
 }
 
+void ShowArray(int* parr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(parr + i) << ' ';
+	}
+	cout << endl;
+}
+
 void InitArray(int* parr, int size)
 {
 	for (int i = 0; i < size; i++)
@@ -41,25 +70,70 @@ void InitArray(int* parr, int size)
 	}
 }
 
-//void InsertSort(int arr[], int size, int value = 1)
-//{
-//	//11 4 2 7 3 11 8 9 6 
-//	int temp;
-//	int j;
-//	for (int i = 0; i < size; i++)
-//	{
-//		//i = 1;????
-//		temp = arr[i];//4
-//		for (j = i - 1; j >= 0 && arr[j] > temp; j--)
-//		{
-//			//j = 0
-//			//arr[j + 1] = 4
-//			arr[j + 1] = arr[j];
-//		}
-//		arr[j + 1] = temp;
-//	}
-//
-//}
+void InitArray2(int* parr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		*(parr + i) = rand() % 41 - 20;
+	}
+}
+
+void InsertSort(int arr[], int size, int value)
+{
+	if (value != 1 && value != 0) value = 1;
+	if (value == 0)
+	{
+		int temp;
+		int j;
+		for (int i = 0; i < size; i++)
+		{
+			temp = arr[i];
+			for (j = i - 1; j >= 0 && arr[j] > temp; j--)
+			{
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+	else if (value == 1)
+	{
+		int temp;
+		int j;
+		for (int i = 0; i < size; i++)
+		{
+			temp = arr[i];
+			for (j = i - 1; j >= 0 && arr[j] < temp; j--)
+			{
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
+
+int* Function(int arr[], int size)
+{
+	int maxElement, minElement;
+    for (minElement = size - 1; minElement >= 0; minElement--) if (arr[minElement] < 0) break;
+    for (maxElement = 0; maxElement <= size - 1; maxElement++) if (arr[maxElement] < 0) break;
+	return InsertSort2(arr, maxElement - maxElement, minElement + 1);
+}
+
+int* InsertSort2(int arr[], int size, int minElement)
+{
+	int temp;
+	int j;
+	for (int i = minElement; i < size - 1; i++)
+	{
+		temp = arr[i];
+		for (j = i - 1; j >= minElement && arr[j] > temp; j--)
+		{
+			arr[j + 1] = arr[j];
+		}
+		arr[j + 1] = temp;
+	}
+	return arr;
+}
 
 int PowerOfNumber(int number, int pow)
 {
--- CPlusPlus/2024-03-18 (Lenears search, Sort)/Header.h
+++ CPlusPlus/2024-03-18 (Lenears search, Sort)/Header.h
@@ -1,4 +1,8 @@
 #pragma once
 int PowerOfNumber(int number, int pow);
 void InitArray(int* arr, int size);
+void ShowArray(int* parr, int size);
 void InsertSort(int arr[], int size, int value = 1);
+int* InsertSort2(int arr[], int size, int minElement);
+void InitArray2(int* parr, int size);
+int* Function(int arr[], int size);
