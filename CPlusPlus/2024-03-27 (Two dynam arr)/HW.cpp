18ca2f36f8d0dd4b5807571bbe484e00edb8b3f7
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-03-30 12:18:51 -0700

    HW (TwoDynamArr)

--- CPlusPlus/2024-03-27 (Two dynam arr)/HW.cpp
+++ CPlusPlus/2024-03-27 (Two dynam arr)/HW.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 int main()
 {
-	int* size = new int;
-	int* arr = CreateArray
+	int* rows = new int;
+	int* cols = new int;
+	cout << "Enter matrix rows: ";
+	cin >> *rows;
+	cout << "Enter matrix cols: ";
+	cin >> *cols;
+	int** arr = CreateArray(rows);
+
+
+
+	for (int i = 0; i < *rows; i++)
+	{
+		delete[] * (arr + i);
+	}
+	delete[] arr;
+}
+
+int** CreateArray(int* size)
+{
+	int** arr = new int* [10];
+	return arr;
 }
