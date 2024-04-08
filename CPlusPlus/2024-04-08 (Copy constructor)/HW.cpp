7c8b7e2966d5737f985dca43d915118856960bcf
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-04-08 19:07:55 +0300

    Lesson (CopyConstructor)

---
+++ CPlusPlus/2024-04-08 (Copy constructor)/HW.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct Carriage
+{
+	int number;
+	int places;
+	int passangers;
+};
+class Train
+{
+	string model;
+	int countCarriager;
+	Carriage* carriages;
+};
+
+int main()
+{
+
+}
