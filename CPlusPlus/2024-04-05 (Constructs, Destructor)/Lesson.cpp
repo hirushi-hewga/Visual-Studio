28cc422bb08b51e1dfd02c2e8e578ec3ce564068
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-04-12 17:35:15 -0700

    Lesson (ConstructsDestructor)

--- CPlusPlus/2024-04-05 (Constructs, Destructor)/Lesson.cpp
+++ CPlusPlus/2024-04-05 (Constructs, Destructor)/Lesson.cpp
@@ -1,107 +1,110 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
+//
+//class Student
+//{
+//	string name;
+//	int age;
+//	int countMarks;
+//	int* marks;
+//	int averageMark;
+//	void SetAverage()
+//	{
+//		int summa = 0;
+//		for (int i = 0; i < countMarks; i++)
+//		{
+//			summa += marks[i];
+//		}
+//		averageMark = (float)summa / countMarks;
+//	}
+//public:
+//	Student() // constructor
+//	{
+//		cout << "Constructor........" << endl;
+//		name = "None";
+//		age = 0;
+//		countMarks = 0;
+//		marks = nullptr;
+//		averageMark = 0;
+//	}
+//	Student(string name, int age)
+//	{
+//		cout << "Constructor........" << endl;
+//		this->name = name;
+//		this->age = age;
+//		countMarks = 0;
+//		marks = nullptr;
+//		averageMark = 0;
+//	}
+//	/*void Initialize()
+//	{
+//		name = "None";
+//		age = 0;
+//		countMarks = 0;
+//		marks = nullptr;
+//		averageMark = 0;
+//	}*/
+//	/*void Initialize(string name, int age)
+//	{
+//		this->name = name;
+//		this->age = age;
+//		countMarks = 0;
+//		marks = nullptr;
+//		averageMark = 0;
+//	}*/
+//	void Show()
+//	{
+//		cout << "Name : " << name << endl;
+//		cout << "Age : " << age << endl;
+//		cout << "Count marks : " << countMarks << endl;
+//		cout << "Marks : ";
+//		for (int i = 0; i < countMarks; i++)
+//		{
+//			cout << marks[i] << ' ';
+//		}
+//		cout << endl;
+//		cout << "AverageMarks : " << averageMark << endl;
+//		cout << endl;
+//	}
+//	void AddMark(int mark)
+//	{
+//		int* temp = new int[countMarks + 1];
+//		for (int i = 0; i < countMarks; i++)
+//		{
+//			temp[i] = marks[i];
+//		}
+//		temp[countMarks] = mark;
+//		if (marks != nullptr)
+//			delete[] marks;
+//		marks = temp;
+//		countMarks++;
+//		SetAverage();
+//	}
+//	void SetName(string name)
+//	{
+//		this->name = name;
+//	}
+//	void SetAge(int age)
+//	{
+//		this->age = age;
+//	}
+//	void DeleteMarks()
+//	{
+//		if (marks != nullptr)
+//			delete[] marks;
+//	}
+//	~Student()
+//	{
+//		cout << "Destructor" << name << endl;
+//		DeleteMarks();
+//	}
+//};
 
-class Student
-{
-	string name;
-	int age;
-	int countMarks;
-	int* marks;
-	int averageMark;
-	void SetAverage()
-	{
-		int summa = 0;
-		for (int i = 0; i < countMarks; i++)
-		{
-			summa += marks[i];
-		}
-		averageMark = (float)summa / countMarks;
-	}
-public:
-	Student() // constructor
-	{
-		cout << "Constructor........" << endl;
-		name = "None";
-		age = 0;
-		countMarks = 0;
-		marks = nullptr;
-		averageMark = 0;
-	}
-	Student(string name, int age)
-	{
-		cout << "Constructor........" << endl;
-		this->name = name;
-		this->age = age;
-		countMarks = 0;
-		marks = nullptr;
-		averageMark = 0;
-	}
-	/*void Initialize()
-	{
-		name = "None";
-		age = 0;
-		countMarks = 0;
-		marks = nullptr;
-		averageMark = 0;
-	}*/
-	/*void Initialize(string name, int age)
-	{
-		this->name = name;
-		this->age = age;
-		countMarks = 0;
-		marks = nullptr;
-		averageMark = 0;
-	}*/
-	void Show()
-	{
-		cout << "Name : " << name << endl;
-		cout << "Age : " << age << endl;
-		cout << "Count marks : " << countMarks << endl;
-		cout << "Marks : ";
-		for (int i = 0; i < countMarks; i++)
-		{
-			cout << marks[i] << ' ';
-		}
-		cout << endl;
-		cout << "AverageMarks : " << averageMark << endl;
-		cout << endl;
-	}
-	void AddMark(int mark)
-	{
-		int* temp = new int[countMarks + 1];
-		for (int i = 0; i < countMarks; i++)
-		{
-			temp[i] = marks[i];
-		}
-		temp[countMarks] = mark;
-		if (marks != nullptr)
-			delete[] marks;
-		marks = temp;
-		countMarks++;
-		SetAverage();
-	}
-	void SetName(string name)
-	{
-		this->name = name;
-	}
-	void SetAge(int age)
-	{
-		this->age = age;
-	}
-	void DeleteMarks()
-	{
-		if (marks != nullptr)
-			delete[] marks;
-	}
-	~Student()
-	{
-		cout << "Destructor" << name << endl;
-		DeleteMarks();
-	}
-};
 
-int main()
-{
+
+
+//int main()
+//{
 	//Student st1;
 	//st1.SetName("Vasia");
 	//st1.Show();
@@ -129,4 +132,4 @@ int main()
 	//st2.AddMark(9);
 	//st2.AddMark(3);
 	//st2.Show();
-}
+//}
