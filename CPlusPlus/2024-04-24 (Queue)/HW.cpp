98ea58ec02cb1db61fc7f00474627fba3ff9cbba
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-04 21:12:53 -0700

    HW (Queue)

--- CPlusPlus/2024-04-24 (Queue)/HW.cpp
+++ CPlusPlus/2024-04-24 (Queue)/HW.cpp
@@ -39,7 +39,7 @@ class Queue
 	int top;
 public:
     Queue() = delete;
-    Queue(Point p):maxSize(3)
+    Queue(Point p):maxSize(5)
     {
         this->p.x = p.x;
         this->p.y = p.y;
@@ -100,7 +100,7 @@ public:
     }
     int GetValue()
     {
-        return arr[1];
+        return arr[2];
     }
 
 
@@ -117,33 +117,37 @@ int main()
     srand(time(NULL));
     bool isTry = true;
 
-    gotoxy(0, 1);
+    gotoxy(0, 3);
     cout << ">";
-    gotoxy(8, 1);
+    gotoxy(8, 3);
     cout << "<";
+    gotoxy(1, 0);
+    cout << "-------";
+    gotoxy(1, 6);
+    cout << "-------";
 
     while (isTry)
     {
         isTry = false;
-        Queue q1(Point(2, 0));
+        Queue q1(Point(2, 1));
         q1.Print();
-        Queue q2(Point(4, 0));
+        Queue q2(Point(4, 1));
         q2.Print();
-        Queue q3(Point(6, 0));
+        Queue q3(Point(6, 1));
         q3.Print();
-        for (int i = 0; i < 15; i++)
+        for (int i = 0; i < 30; i++)
         {
-            if (i < 5) q1.Dequeue();
-            if (i < 10) q2.Dequeue();
+            if (i < 10) q1.Dequeue();
+            if (i < 20) q2.Dequeue();
             q3.Dequeue();
-            Sleep(400);
+            Sleep(200);
             q1.Print();
             q2.Print();
             q3.Print();
         }
-        gotoxy(0, 4);
+        gotoxy(0, 7);
         cout << "Res : " << q1.GetValue() << q2.GetValue() << q3.GetValue() << endl;
-        gotoxy(0, 6);
+        gotoxy(0, 9);
         if (q1.GetValue() == q2.GetValue() && q2.GetValue() == q3.GetValue())
             cout << "You won!";
         else
@@ -151,12 +155,12 @@ int main()
         char symbol = 'a';
         while (symbol != 'y' && symbol != 'n')
         {
-            gotoxy(0, 7);
+            gotoxy(0, 10);
             cout << "Try again ? y/n : "; symbol = _getch();
             if (symbol == 'y') isTry = true;
             else if (symbol == 'n')
             {
-                gotoxy(0, 9);
+                gotoxy(0, 12);
                 cout << "Good bye." << endl;
             }
         }
