9372c4b85095361c5995e0bc2804781122270772
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-06 04:38:38 -0700

    HW2 (QueueWithPriority)

---
+++ CPlusPlus/2024-04-26 (Queue with priority)/HW2.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+	enum { EMPTY = -1 };
+	int size;
+	char* arr;
+	int top;
+public:
+	Stack() = delete;
+	Stack(int size)
+	{
+		this->size = size;
+		arr = new char[size];
+		top = EMPTY;
+	}
+
+
+	bool IsFull()const
+	{
+		return top < size - 1;
+	}
+	bool IsEmpty()const
+	{
+		return top == EMPTY;
+	}
+	void Push(char symbol)
+	{
+		if (!IsFull())
+		{
+			arr[++top] = symbol;
+		}
+	}
+	void Pop()
+	{
+		if (!IsEmpty())
+		{
+			top--;
+		}
+	}
+	int GetCount()const
+	{
+		return top + 1;
+	}
+	void Clear()
+	{
+		top = EMPTY;
+	}
+	char Peek()const
+	{
+		return arr[top];
+	}
+	void ChangeMaxSize(int size)
+	{
+		if (size > 0)
+		{
+			char* temp = new char[size];
+			int newTop = top < size ? top : size;
+			if (top != EMPTY)
+			{
+				for (int i = 0; i <= newTop; i++)
+				{
+					temp[i] = arr[i];
+				}
+				delete[] arr;
+			}
+			arr = temp;
+			top = newTop;
+			this->size = size;
+		}
+	}
+
+
+	~Stack()
+	{
+		if (arr != nullptr)
+			delete[] arr;
+	}
+	friend ostream& operator <<(ostream& out, const Stack& other);
+};
+
+ostream& operator <<(ostream& out, const Stack& other)
+{
+	for (int i = 0; i <= other.top; i++)
+	{
+		out << other.arr[i];
+	}
+	return out;
+}
+
+
+
+int main()
+{
+	Stack st(8);
+	st.Push('a');
+	st.Push('b');
+	st.Push('c');
+	st.Push('d');
+	st.Push('e');
+	cout << st << endl;
+}
