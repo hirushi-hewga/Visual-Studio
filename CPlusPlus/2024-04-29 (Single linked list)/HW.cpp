3792860abd505cee0695ed6e3dbb3a473b89d8b8
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-09 03:17:34 -0700

    HW (SingleLinkedList)

--- CPlusPlus/2024-04-29 (Single linked list)/HW.cpp
+++ CPlusPlus/2024-04-29 (Single linked list)/HW.cpp
@@ -17,13 +17,18 @@ public:
 	List():head(nullptr) {}
 	List(const List& other)
 	{
-		Node<T>* current = this->head;
-		other.head = nullptr;
-		while (current != nullptr)
+		Node<T>* newNode = new Node<T>(other.head->value, nullptr);
+		this->head = newNode;
+
+		
+		Node<T>* current_other = other.head;
+		Node<T>* current_this = this->head;
+		while (current_other->next != nullptr)
 		{
-			Node<T>* newNode = new Node(current->value, other.head);
-			other.head = newNode;
-			current = current->next;
+			Node<T>* newNode = new Node<T>(current_other->next->value, nullptr);
+			current_this->next = newNode;
+			current_this = current_this->next;
+			current_other = current_other->next;
 		}
 	}
 	List operator =(const List& other)
@@ -106,43 +111,58 @@ public:
 		while (current != nullptr)
 		{
 			if (pos == ++i) return current->value;
+			current = current->next;
 		}
 		return 0;
 	}
 	void Add(int pos, T value)
 	{
-		Node<T>* current = head;
-		int i = 0;
-		while (current != nullptr)
+		if (pos == 1)
 		{
-			if (pos - 1 == ++i)
+			AddToHead(value);
+		}
+		else
+		{
+			Node<T>* current = head;
+			int i = 0;
+			while (current != nullptr)
 			{
-				Node<T>* newNode = new Node<T>(value, current->next);
-				current->next = newNode;
+				if (pos - 1 == ++i)
+				{
+					Node<T>* newNode = new Node<T>(value, current->next);
+					current->next = newNode;
+				}
+				current = current->next;
 			}
-			current = current->next;
 		}
 	}
 	void Delete(int pos)
 	{
-		Node<T>* current = head;
-		int i = 0;
-		while (current != nullptr)
+		if (pos == 1)
 		{
-			if (pos - 1 == ++i)
+			DeleteFromHead();
+		}
+		else
+		{
+			Node<T>* current = head;
+			int i = 0;
+			while (current != nullptr)
 			{
-				Node<T>* next = current->next->next;
-				delete current->next;
-				current->next = next;
+				if (pos - 1 == ++i)
+				{
+					Node<T>* next = current->next->next;
+					delete current->next;
+					current->next = next;
+				}
+				current = current->next;
 			}
-			current = current->next;
 		}
 	}
 
 
 	~List()
 	{
-		Node* current = nullptr;
+		Node<T>* current = nullptr;
 		while (head != nullptr)
 		{
 			current = head;
@@ -157,10 +177,44 @@ public:
 int main()
 {
 	srand(time(NULL));
-	List<int>* list;
+	List<int> list;
+	list.AddToHead(888);
+	cout << "list 1 : "; list.Show();
 	for (int i = 0; i < 5; i++)
 	{
-		list->AddToHead(rand() % 9 + 1);
+		list.AddToHead(rand() % 9 + 1);
 	}
-	list->Show();
+	cout << "list 1 : "; list.Show();
+	for (int i = 0; i < 5; i++)
+	{
+		list.AddToTail(rand() % 10 + 10);
+	}
+	cout << "list 1 : "; list.Show();
+	list.DeleteFromHead();
+	list.DeleteFromHead();
+	cout << "list 1 : "; list.Show();
+	list.DeleteFromTail();
+	list.DeleteFromTail();
+	cout << "list 1 : "; list.Show();
+	cout << "Element 1 = " << list.Find(1) << endl;
+	cout << "Element 3 = " << list.Find(3) << endl;
+	cout << "Element 6 = " << list.Find(6) << endl;
+	List<int> list2(list);
+	cout << "list 2 : "; list2.Show();
+	List<int> list3 = list2;
+	cout << endl;
+	cout << "list 1 : "; list.Show();
+	cout << "list 2 : "; list2.Show();
+	cout << "list 3 : "; list3.Show();
+
+	list.Add(1, 1111);
+	list2.Add(3, 1111);
+	list3.Add(5, 1111);
+	list.Delete(2);
+	list2.Delete(4);
+	list3.Delete(6);
+	cout << endl;
+	cout << "list 1 : "; list.Show();
+	cout << "list 2 : "; list2.Show();
+	cout << "list 3 : "; list3.Show();
 }
