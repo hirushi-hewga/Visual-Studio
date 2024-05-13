97f9020e7482c44a9a3ab3ac4fd1e23a7b8d8b16
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-13 05:42:11 -0700

    HW_1 (Inheritance)

--- CPlusPlus/2024-05-01 (Inheritance)/HW.cpp
+++ CPlusPlus/2024-05-01 (Inheritance)/HW.cpp
@@ -14,13 +14,14 @@ class Student
 protected:
 	Fullname fullname;
 	int age;
+public:
 	Student() = delete;
 	Student(string name, string surname, string lastname, int age):fullname(name, surname, lastname), age(age) {}
 
 
 	void Print()const
 	{
-		cout << "Fullname : " << fullname.name << ' ' << fullname.surname << ' ' << fullname.lastname << endl;
+		cout << "Fullname : " << fullname.surname << ' ' << fullname.name << ' ' << fullname.lastname << endl;
 		cout << "Age : " << age << endl;
 	}
 };
@@ -28,14 +29,26 @@ protected:
 class Aspirant : public Student
 {
 	int sheets;
+	int death_count;
 public:
-	Aspirant(string name, string surname, string lastname, int age, int sheets):sheets(sheets), Student(name, surname, lastname, age) {}
+	Aspirant(string name, string surname, string lastname, int age, int sheets):sheets(sheets), death_count(sheets/8), Student(name, surname, lastname, age) {}
 
+
+	void Print()
+	{
+		Student::Print();
+		cout << "Sheets write : " << sheets << endl;
+		cout << "Death count : " << death_count << endl;
+	}
 };
 
 
 
 int main()
 {
+	Student st("Bogdan", "Kurylo", "Andriyovych", 17);
+	Aspirant asp("Vladyslav", "Yanchuk", "Maksymovych", 17, 99);
 
+	cout << "============ Student ============" << endl;st.Print();
+	cout << "============ Aspirant ===========" << endl;asp.Print();
 }
