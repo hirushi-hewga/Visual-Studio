f2a323d783ca9ce4dc62cc393db08279165b27d4
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-21 05:27:27 -0700

    HW_2 (InheritancePolimorphism)

--- CPlusPlus/2024-05-13 (Inheritance, Polimorphism)/HW2.cpp
+++ CPlusPlus/2024-05-13 (Inheritance, Polimorphism)/HW2.cpp
@@ -9,11 +9,11 @@ public:
 	Shape():name("no name") {}
 	Shape(string n):name(n) {}
 
+	virtual float GetArea()const = 0 {};
 	virtual void Show()const
 	{
 		cout << "Name : " << name << endl;
 	}
-	virtual void GetArea()const = 0 {};
 };
 
 class Rectangle : public Shape
@@ -22,18 +22,23 @@ class Rectangle : public Shape
 	float side_2;
 public:
 	Rectangle():side_1(0), side_2(0), Shape() {}
-	Rectangle(string n, float s1, float s2):side_1(s1),side_2(s2),Shape(n) {}
+	Rectangle(string n, float s1, float s2) : Shape(n)
+	{
+		this->side_1 = s1 > 0 ? s1 : 0;
+		this->side_2 = s2 > 0 ? s2 : 0;
+	}
 
+	float GetArea()const override
+	{
+		return side_1 * side_2;
+	}
 	void Show()const override
 	{
 		cout << "======== Rectangle ========" << endl;
 		Shape::Show();
 		cout << "Side 1 : " << side_1 << " sm" << endl;
 		cout << "Side 2 : " << side_2 << " sm" << endl;
-	}
-	void GetArea()const override
-	{
-		return side_1 * side_2;
+		cout << "Area : " << GetArea() << "sm ^ 2" << endl;
 	}
 };
 
@@ -42,17 +47,21 @@ class Circle : public Shape
 	float radius;
 public:
 	Circle() :radius(0), Shape() {}
-	Circle(string n, float r) :radius(r), Shape(n) {}
+	Circle(string n, float r) : Shape(n)
+	{
+		this->radius = r > 0 ? r : 0;
+	}
 
+	float GetArea()const override
+	{
+		return pow(3.14 * radius, 2);
+	}
 	void Show()const override
 	{
 		cout << "======== Circle ========" << endl;
 		Shape::Show();
 		cout << "Radius : " << radius << " sm" << endl;
-	}
-	void GetArea()const override
-	{
-		return pow(3.14 * radius, 2);
+		cout << "Area : " << GetArea() << "sm ^ 2" << endl;
 	}
 };
 
@@ -62,18 +71,23 @@ class RightTriangle : public Shape
 	float katet_2;
 public:
 	RightTriangle() :katet_1(0), katet_2(0), Shape() {}
-	RightTriangle(string n, float k1, float k2) :katet_1(k1), katet_2(k2), Shape(n) {}
+	RightTriangle(string n, float k1, float k2) : Shape(n)
+	{
+		this->katet_1 = k1 > 0 ? k1 : 0;
+		this->katet_2 = k2 > 0 ? k2 : 0;
+	}
 
+	float GetArea()const override
+	{
+		return (katet_1 * katet_2) / 2;
+	}
 	void Show()const override
 	{
 		cout << "======== Right Triangle ========" << endl;
 		Shape::Show();
 		cout << "Katet 1 : " << katet_1 << " sm" << endl;
 		cout << "Katet 2 : " << katet_2 << " sm" << endl;
-	}
-	void GetArea()const override
-	{
-		return (katet_1 * katet_2) / 2;
+		cout << "Area : " << GetArea() << "sm ^ 2" << endl;
 	}
 };
 
@@ -84,8 +98,18 @@ class Triangle : public Shape
 	float side_3;
 public:
 	Triangle() :side_1(0), side_2(0), side_3(0), Shape() {}
-	Triangle(string n, float s1, float s2, float s3) :side_1(s1), side_2(s2), side_3(s3), Shape(n) {}
+	Triangle(string n, float s1, float s2, float s3) : Shape(n)
+	{
+		this->side_1 = s1 > 0 ? s1 : 0;
+		this->side_2 = s2 > 0 ? s2 : 0;
+		this->side_3 = s3 > 0 ? s3 : 0;
+	}
 
+	float GetArea()const override
+	{
+		double p = (side_1 + side_2 + side_3) / 2;
+		return sqrt(p * (p - side_1) * (p - side_2) * (p - side_3));
+	}
 	void Show()const override
 	{
 		cout << "======== Triangle ========" << endl;
@@ -93,17 +117,54 @@ public:
 		cout << "Side 1 : " << side_1 << " sm" << endl;
 		cout << "Side 2 : " << side_2 << " sm" << endl;
 		cout << "Side 3 : " << side_3 << " sm" << endl;
+		cout << "Area : " << GetArea() << "sm ^ 2" << endl;
 	}
-	void GetArea()const override
+};
+
+class Trapeze : public Shape
+{
+	float height;
+	float base_1;
+	float base_2;
+public:
+	Trapeze() :height(0), base_1(0), base_2(0), Shape() {}
+	Trapeze(string n, float h, float b1, float b2) : Shape(n)
 	{
-		double p = (side_1 + side_2 + side_3) / 2;
-		return sqrt(p * (p - side_1) * (p - side_2) * (p - side_3));
+		this->height = h > 0 ? h : 0;
+		this->base_1 = b1 > 0 ? b1 : 0;
+		this->base_2 = b2 > 0 ? b2 : 0;
+	}
+
+	float GetArea()const override
+	{
+		return (base_1 + base_2) * height / 2;
+	}
+	void Show()const override
+	{
+		cout << "======== Triangle ========" << endl;
+		Shape::Show();
+		cout << "Height : " << height << " sm" << endl;
+		cout << "Base 1 : " << base_1 << " sm" << endl;
+		cout << "Base 2 : " << base_2 << " sm" << endl;
+		cout << "Area : " << GetArea() << "sm ^ 2" << endl;
 	}
 };
 
 
 
+void TestShape(Shape& s) { s.Show(); }
+
 int main()
 {
+	Rectangle rect("Rectangle", 5, 7);
+	Circle circle("Circle", 7);
+	RightTriangle rt("Right triangle", 5, 7);
+	Triangle triangle("Triangle", 5, 7, 9);
+	Trapeze trap("Trapeze", 5, 7, 9);
 
+	TestShape(rect);
+	TestShape(circle);
+	TestShape(rt);
+	TestShape(triangle);
+	TestShape(trap);
 }
