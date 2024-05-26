b1969bc77de85dcef86212ea42aaf8207f218fb2
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-26 05:43:23 -0700

    Lesson (AgregationComposition)

--- CPlusPlus/2024-05-20 (Agregation, Composition)/Lesson.cpp
+++ CPlusPlus/2024-05-20 (Agregation, Composition)/Lesson.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Carteidge
+class Cartridge
 {
 public:
 	enum TYPE { NONE, STANDART, L, XL };
@@ -10,24 +10,96 @@ private:
 	float volume;
 	TYPE type;
 public:
-	Carteidge() : color("no color"), volume(0), type(NONE) {}
-	Carteidge(string c, float v, TYPE t) : color(c), volume(v), type(t) {}
+	Cartridge() : color("no color"), volume(0), type(NONE) {}
+	Cartridge(string c, float v, TYPE t) : color(c), volume(v), type(t) {}
 
-	void Show()
+	void Show()const
 	{
 		cout << "Color : " << color << endl;
 		cout << "Volume : " << volume << endl;
+		cout << "Type : ";
+		switch (type)
+		{
+		case Cartridge::NONE: cout << "None" << endl; break;
+		case Cartridge::STANDART: cout << "Standart" << endl; break;
+		case Cartridge::L: cout << "L" << endl; break;
+		case Cartridge::XL: cout << "XL" << endl; break;
+		}
+		cout << endl;
 	}
 };
 
+class Scanner
+{
+	float height, width;
+	float scanResolution;
+public:
+	Scanner() : height(0), width(0), scanResolution(0) {}
+	Scanner(float h, float w, float s) : height(h), width(w), scanResolution(s) {}
+
+	void ShowInfo()const
+	{
+		cout << "Scanner with " << scanResolution << " dpi" << endl;
+		cout << "Area : " << height << " mm ,  " << width << " mm" << endl;
+	}
+};
+
+
+
 class Printer
 {
+	string model;
+	int year;
+	Cartridge* cartridges;
+	int cartridgeCount;
+	Scanner scanner;
+public:
+	Printer() : model("no model"), year(0), cartridges(nullptr), cartridgeCount(0) {}
+	Printer(string model, int year, float h, float w, float scanR)
+	{
+		this->model = model;
+		this->year = year;
 
+		scanner = Scanner(h, w, scanR);
+
+		cartridgeCount = 4;
+		cartridges = new Cartridge[cartridgeCount];
+		cartridges[0] = Cartridge("Black", 500, Cartridge::XL);
+		cartridges[1] = Cartridge("Blue", 300, Cartridge::L);
+		cartridges[2] = Cartridge("Green", 200, Cartridge::L);
+		cartridges[3] = Cartridge("Red", 100, Cartridge::L);
+	}
+	void Scan()const
+	{
+		cout << "Scanning..........." << endl;
+		scanner.ShowInfo();
+	}
+	void PrintDocument()const
+	{
+		cout << "Printing document" << endl;
+	}
+	void ShowProperties()const
+	{
+		cout << "Printing with setting : " << endl;
+		for (int i = 0; i < cartridgeCount; i++)
+		{
+			cartridges[i].Show();
+		}
+	}
+
+	~Printer()
+	{
+		if (cartridges != nullptr)
+			delete[] cartridges;
+	}
 };
 
 
 
 int main()
 {
-
+	Printer printer("Cannon", 2020, 50, 50, 300);
+	printer.Scan();
+	printer.PrintDocument();
+	printer.ShowProperties();
 }
