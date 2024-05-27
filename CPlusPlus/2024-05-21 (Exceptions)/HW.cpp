4c8b186efb8f496c6c496bd1af20c0c5a803b69f
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-27 03:59:45 -0700

    HW (Exceptions)

--- CPlusPlus/2024-05-21 (Exceptions)/HW.cpp
+++ CPlusPlus/2024-05-21 (Exceptions)/HW.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <Windows.h>
+#include <conio.h>
 using namespace std;
 
 struct Node
 {
 	Node* prev;
-	int value;
+	float value;
 	Node* next;
-	Node(Node* prev, int value, Node* next):prev(prev),value(value), next(next) {}
+	Node(Node* prev, float value, Node* next):prev(prev),value(value), next(next) {}
 };
 
 class List
@@ -22,7 +22,7 @@ public:
 	{
 		return head == nullptr;
 	}
-	void AddToHead(int value)
+	void AddToHead(float value)
 	{
 		Node* newNode = new Node(nullptr, value, nullptr);
 		if (IsEmpty())
@@ -36,7 +36,7 @@ public:
 			head = newNode;
 		}
 	}
-	void AddToTail(int value)
+	void AddToTail(float value)
 	{
 		Node* newNode = new Node(nullptr, value, nullptr);
 		if (IsEmpty())
@@ -82,6 +82,8 @@ public:
 	}
 	void Show()const
 	{
+		if (IsEmpty()) throw exception("List is empty");
+		system("cls");
 		Node* current = head;
 		while (current != nullptr)
 		{
@@ -101,13 +103,95 @@ int Menu()
 	while (choice < 1 || choice > 6)
 	{
 		system("cls");
+		if (!isValidData)
+		{
+			cout << "Error choice! Try again." << endl;
+			cout << endl;
+		}
+		cout << "1 - Add value to head" << endl;
+		cout << "2 - Add value to tail" << endl;
+		cout << "3 - Delete value from head" << endl;
+		cout << "4 - Delete value from tail" << endl;
+		cout << "5 - Show list" << endl;
+		cout << "6 - Exit" << endl;
+		cout << "Enter your choice : ";
+		cin >> choice;
 	}
+	return choice;
 }
 enum MENU { ADD_TO_HEAD = 1, ADD_TO_TAIL, DELETE_FROM_HEAD, DELETE_FROM_TAIL, PRINT_LIST, EXIT };
-int main()
-{
-	cout << "123\n123\n123\n123" << endl;
-	Sleep(1000);
-	system("cls");
-	cout << "123\n123\n123\n123" << endl;
-}
+//int main()
+//{
+	/*char s;
+	List list;
+	bool isExit = false;
+	while (!isExit)
+	{
+		try
+		{
+			float value;
+			switch (Menu())
+			{
+			case ADD_TO_HEAD:
+				system("cls");
+				cout << "Enter value : "; cin >> value;
+				cout << endl;
+				list.AddToHead(value);
+				cout << "Value added to head" << endl;
+				cout << endl;
+				cout << "Press any key to continue : ";
+				s = _getch();
+				break;
+			case ADD_TO_TAIL:
+				system("cls");
+				cout << "Enter value : "; cin >> value;
+				cout << endl;
+				list.AddToTail(value);
+				cout << "Value added to tail" << endl;
+				cout << endl;
+				cout << "Press any key to continue : ";
+				s = _getch();
+				break;
+			case DELETE_FROM_HEAD:
+				system("cls");
+				list.DeleteFromHead();
+				cout << "Value deleted from head" << endl;
+				cout << endl;
+				cout << "Press any key to continue : ";
+				s = _getch();
+				break;
+			case DELETE_FROM_TAIL:
+				system("cls");
+				list.DeleteFromTail();
+				cout << "Value deleted from tail" << endl;
+				cout << endl;
+				cout << "Press any key to continue : ";
+				s = _getch();
+				break;
+			case PRINT_LIST:
+				list.Show();
+				cout << endl;
+				cout << "Press any key to continue : ";
+				s = _getch();
+				break;
+			case EXIT: isExit = true;
+			}
+		}
+		catch (exception ex)
+		{
+			system("cls");
+			cout << ex.what() << endl;
+			cout << endl;
+			cout << "Press any key to continue : ";
+			s = _getch();
+		}
+		catch (...)
+		{
+			system("cls");
+			cout << "Unknown exception" << endl;
+			cout << endl;
+			cout << "Press any key to continue : ";
+			s = _getch();
+		}
+	}*/
+//}
