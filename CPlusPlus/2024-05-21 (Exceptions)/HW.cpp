e7af60c5b7631031227e464293ddcf883c3b9d07
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-27 03:27:29 -0700

    Lesson (Exceptions)

--- CPlusPlus/2024-05-21 (Exceptions)/HW.cpp
+++ CPlusPlus/2024-05-21 (Exceptions)/HW.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 struct Node
 {
 	Node* prev;
-	int num;
+	int value;
 	Node* next;
-	Node(Node* prev, int num, Node* next):prev(prev),num(num), next(next) {}
+	Node(Node* prev, int value, Node* next):prev(prev),value(value), next(next) {}
 };
 
 class List
@@ -52,17 +53,61 @@ public:
 	void DeleteFromHead()
 	{
 		if (IsEmpty()) throw exception("List is empty");
-		if (head == tail)
+		else if (head == tail)
 		{
 			delete head;
 			head = tail = nullptr;
 		}
+		else
+		{
+			head = head->next;
+			delete head->prev;
+			head->prev = nullptr;
+		}
+	}
+	void DeleteFromTail()
+	{
+		if (IsEmpty()) throw exception("List is empty");
+		else if (head == tail)
+		{
+			delete head;
+			head = tail = nullptr;
+		}
+		else
+		{
+			tail = tail->prev;
+			delete tail->next;
+			tail->next = nullptr;
+		}
+	}
+	void Show()const
+	{
+		Node* current = head;
+		while (current != nullptr)
+		{
+			cout << current->value << ' ';
+			current = current->next;
+		}
+		cout << endl;
 	}
 };
 
 
 
+int Menu()
+{
+	int choice = 0;
+	bool isValidData = true;
+	while (choice < 1 || choice > 6)
+	{
+		system("cls");
+	}
+}
+enum MENU { ADD_TO_HEAD = 1, ADD_TO_TAIL, DELETE_FROM_HEAD, DELETE_FROM_TAIL, PRINT_LIST, EXIT };
 int main()
 {
-
+	cout << "123\n123\n123\n123" << endl;
+	Sleep(1000);
+	system("cls");
+	cout << "123\n123\n123\n123" << endl;
 }
--- CPlusPlus/2024-05-21 (Exceptions)/Lesson.cpp
+++ CPlusPlus/2024-05-21 (Exceptions)/Lesson.cpp
@@ -1,82 +1,82 @@
-#include <iostream>
-//#include <exception>
-using namespace std;
-
-class List
-{
-	int size;
-public:
-	List() : size(0) {}
-	List(int size)
-	{
-		this->size = size > 0 ? size : 0;
-	}
-
-	int GetFromPosition(int pos)const
-	{
-		// 1 - code - collection is empty
-		if (size == 0) return 1;
-		// 2 - code - out of range
-		if (pos < 1 || pos > size) return 2;
-
-		return rand() % 10;
-	}
-};
-
-float Divide(float a, float b)
-{
-	// try catch throw
-	float res = 0;
-	if (b == 0)
-	{
-		cout << "You can`t divide by zero" << endl;
-		throw exception("Divide by zero");
-	}
-	else if (b == 2)
-	{
-		throw 404;
-	}
-	else if (b == 5)
-	{
-		throw "Divide by 5";
-	}
-	else
-	{
-		res = a / b;
-		cout << "Res in function = " << res << endl;
-		return res;
-	}
-}
-
-class PasswordInvalidException : public exception
-{
-public:
-	PasswordInvalidException(const char* ex) : exception(ex) {}
-};
-class PasswordSmallException : public exception
-{
-	int length;
-public:
-	PasswordSmallException(const char* ex, int length) : length(length), exception(ex) {}
-
-	void Message()const
-	{
-		cout << what() << endl;
-		cout << "Actual length : " << length << endl;
-	}
-};
-
-void Login(const char* login, const char* password)
-{
-	if (strlen(password) < 6)
-		throw PasswordSmallException("To small password");
-	if (!isalpha(login[0]) || !islower(login[0]))
-		throw exception("Login is invalid");
-	if (!isalpha(password[0]) || !islower(password[0]))
-		throw PasswordInvalidException("Password invalid");
-
-	cout << "Loading..........." << endl;
-}
+//#include <iostream>
+////#include <exception>
+//using namespace std;
+//
+//class List
+//{
+//	int size;
+//public:
+//	List() : size(0) {}
+//	List(int size)
+//	{
+//		this->size = size > 0 ? size : 0;
+//	}
+//
+//	int GetFromPosition(int pos)const
+//	{
+//		// 1 - code - collection is empty
+//		if (size == 0) return 1;
+//		// 2 - code - out of range
+//		if (pos < 1 || pos > size) return 2;
+//
+//		return rand() % 10;
+//	}
+//};
+//
+//float Divide(float a, float b)
+//{
+//	// try catch throw
+//	float res = 0;
+//	if (b == 0)
+//	{
+//		cout << "You can`t divide by zero" << endl;
+//		throw exception("Divide by zero");
+//	}
+//	else if (b == 2)
+//	{
+//		throw 404;
+//	}
+//	else if (b == 5)
+//	{
+//		throw "Divide by 5";
+//	}
+//	else
+//	{
+//		res = a / b;
+//		cout << "Res in function = " << res << endl;
+//		return res;
+//	}
+//}
+//
+//class PasswordInvalidException : public exception
+//{
+//public:
+//	PasswordInvalidException(const char* ex) : exception(ex) {}
+//};
+//class PasswordSmallException : public exception
+//{
+//	int length;
+//public:
+//	PasswordSmallException(const char* ex, int length) : length(length), exception(ex) {}
+//
+//	void Message()const
+//	{
+//		cout << what() << endl;
+//		cout << "Actual length : " << length << endl;
+//	}
+//};
+//
+//void Login(const char* login, const char* password)
+//{
+//	if (strlen(password) < 6)
+//		throw PasswordSmallException("To small password");
+//	if (!isalpha(login[0]) || !islower(login[0]))
+//		throw exception("Login is invalid");
+//	if (!isalpha(password[0]) || !islower(password[0]))
+//		throw PasswordInvalidException("Password invalid");
+//
+//	cout << "Loading..........." << endl;
+//}
 
 
 
