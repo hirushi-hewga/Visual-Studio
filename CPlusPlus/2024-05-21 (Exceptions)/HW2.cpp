f7e4a27b23bc0454375160d454ba2f4831931990
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-05-28 07:20:28 -0700

    HW (Exceptions)

--- CPlusPlus/2024-05-21 (Exceptions)/HW2.cpp
+++ CPlusPlus/2024-05-21 (Exceptions)/HW2.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 
 
 int CharToInt_(const char* s)
 {
-	if (atoi(s) == 0) return;
+	for (int i = 0; i < strlen(s); i++)
+	{
+		if (!isdigit(s[i]))
+			throw exception("Numerical error");
+	}
+	char* endptr;
+	long long  number = atoll(s);
+	if (number > INT_MAX || number < INT_MIN)
+		throw overflow_error("out of range");
+	return atoi(s);
 }
 
 int main()
 {
-
+	try
+	{
+		int a = CharToInt_("2147483647a");
+		cout << a << endl;
+	}
+	catch (overflow_error ex)
+	{
+		cout << endl;
+		cout << ex.what() << endl;
+		cout << endl;
+	}
+	catch (exception ex)
+	{
+		cout << endl;
+		cout << ex.what() << endl;
+		cout << endl;
+	}
+	catch (...)
+	{
+		cout << "\nUnknown exception\n" << endl;
+	}
 }
