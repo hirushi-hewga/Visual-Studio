d8b3429429ced40f226a7e7b63ca323d8cfcee28
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-01 19:41:39 -0700

    HW (Map_BinaryTree)

--- CPlusPlus/2024-05-29 (Map)/HW.cpp
+++ CPlusPlus/2024-05-29 (Map)/HW.cpp
@@ -19,9 +19,9 @@ public:
 			cout << "Word : " << pair.first << " - ";
 			for (string word : pair.second)
 			{
-				cout << " | " << word;
+				cout << word << ", ";
 			}
-			cout << " |" << endl;
+			cout << endl;
 		}
 	}
 	void AddTranslate(string word)
@@ -41,6 +41,13 @@ public:
 			} while (!translate.empty());
 		}
 	}
+	void AddTranslate_(string word, string translate)
+	{
+		if (dic.find(word) == dic.end())
+			cout << "Word not found!" << endl;
+		else
+			dic[word].push_back(translate);
+	}
 	void AddWord(string word, string translate)
 	{
 		dic.insert(make_pair(word, list<string>{translate}));
@@ -52,7 +59,27 @@ public:
 int main()
 {
 	Dictionary dictionary("English-Ukrainian");
-	dictionary.AddWord("how", "yak");
+	dictionary.AddWord("light", "svitlo");
+	dictionary.AddWord("great", "chudovo");
+	dictionary.AddWord("just", "prosto");
+	dictionary.AddWord("open", "vidchuneno");
+	dictionary.AddWord("neither", "ni");
+	dictionary.AddWord("because", "oskilku");
+	dictionary.AddWord("left", "zliva");
+	dictionary.AddWord("alive", "badioruy");
+	dictionary.AddWord("quality", "yakist");
+	dictionary.AddWord("may", "mozna");
+	dictionary.Print();
+	dictionary.AddTranslate_("light", "lehkiy");
+	dictionary.AddTranslate_("great", "velukiy");
+	dictionary.AddTranslate_("just", "zaraz");
+	dictionary.AddTranslate_("open", "otvir");
+	dictionary.AddTranslate_("neither", "zoden");
+	dictionary.AddTranslate_("because", "bo");
+	dictionary.AddTranslate_("left", "pokunytuy");
+	dictionary.AddTranslate_("alive", "zuvuy");
+	dictionary.AddTranslate_("quality", "vlastuvist");
+	dictionary.AddTranslate_("may", "traven");
 	dictionary.Print();
 
 
