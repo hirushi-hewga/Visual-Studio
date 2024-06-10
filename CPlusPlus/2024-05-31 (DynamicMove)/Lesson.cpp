0d05761d7203186ba77108b21f47126fb46858e5
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-09 19:50:24 -0700

    Lesson (DM)

--- CPlusPlus/2024-05-31 (DynamicMove)/Lesson.cpp
+++ CPlusPlus/2024-05-31 (DynamicMove)/Lesson.cpp
@@ -1,117 +1,117 @@
-#include "Console.h"
-
-enum DIRECTION { LEFT, RIGHT, UP, DOWN };
-
-class Hero
-{
-	string name;
-	int hp;
-	int h, w;
-	COORD position;
-	char symbol;
-	DIRECTION dir;
-
-	void Print(char filler)const
-	{
-		for (int i = 0; i < h; i++)
-		{
-			SetCursorPosition(position.X, position.Y + i);
-			for (int i = 0; i < w; i++)
-			{
-				cout << filler;
-			}
-			cout << endl;
-		}
-	}
-public:
-	Hero() : name("no name"), hp(100), h(1), w(1), position({0, 0}), symbol('#'), dir(RIGHT) {}
-	Hero(string name, int h, int w, char s) : name(name), hp(100), h(h), w(w), position({ 0, 0 }), symbol(s), dir(RIGHT)
-	{
-		this->h = h >= 1 ? h : 1;
-		this->w = w >= 1 ? w : 1;
-	}
-
-	void PrintHero()const
-	{
-		SetConsoleTextAttribute(hConsole, ConsoleColors::GREEN);
-		Print(symbol);
-	}
-	void ClearHero()const
-	{
-		Print(' ');
-	}
-	void MarkHero()const
-	{
-		SetConsoleTextAttribute(hConsole, ConsoleColors::RED);
-		Print(symbol);
-	}
-	void ShowInfo()const
-	{
-		cout << "-------- Player [ " << name << " ] . HP : " << hp << " --------" << endl;
-	}
-	void SetPosition(int x, int y)
-	{
-		position.X = x >= 0 ? x : 0;
-		position.Y = y >= 0 ? y : 0;
-	}
-	void Move()
-	{
-		ClearHero();
-		bool isMoved = false;
-		switch (dir)
-		{
-		case LEFT: isMoved = MoveLeft(); break;
-		case RIGHT: isMoved = MoveRight(); break;
-		case UP: isMoved = MoveUp(); break;
-		case DOWN: isMoved = MoveDown();
-		}
-		isMoved ? PrintHero() : MarkHero();
-	}
-	void ChangeDirection(DIRECTION dir)
-	{
-		this->dir = dir;
-	}
-	bool IsValidPosition(int x, int y)const
-	{
-		return x >= 0 && y >= 0;
-	}
-	bool MoveRight()
-	{
-		if (IsValidPosition(position.X + 1, position.Y))
-		{
-			position.X++;
-			return true;
-		}
-		return false;
-	}
-	bool MoveLeft()
-	{
-		if (IsValidPosition(position.X - 1, position.Y))
-		{
-			position.X--;
-			return true;
-		}
-		return false;
-	}
-	bool MoveUp()
-	{
-		if (IsValidPosition(position.X, position.Y - 1))
-		{
-			position.Y--;
-			return true;
-		}
-		return false;
-	}
-	bool MoveDown()
-	{
-		if (IsValidPosition(position.X, position.Y + 1))
-		{
-			position.Y++;
-			return true;
-		}
-		return false;
-	}
-};
+//#include "Console.h"
+//
+//enum DIRECTION { LEFT, RIGHT, UP, DOWN };
+//
+//class Hero
+//{
+//	string name;
+//	int hp;
+//	int h, w;
+//	COORD position;
+//	char symbol;
+//	DIRECTION dir;
+//
+//	void Print(char filler)const
+//	{
+//		for (int i = 0; i < h; i++)
+//		{
+//			SetCursorPosition(position.X, position.Y + i);
+//			for (int i = 0; i < w; i++)
+//			{
+//				cout << filler;
+//			}
+//			cout << endl;
+//		}
+//	}
+//public:
+//	Hero() : name("no name"), hp(100), h(1), w(1), position({0, 0}), symbol('#'), dir(RIGHT) {}
+//	Hero(string name, int h, int w, char s) : name(name), hp(100), h(h), w(w), position({ 0, 0 }), symbol(s), dir(RIGHT)
+//	{
+//		this->h = h >= 1 ? h : 1;
+//		this->w = w >= 1 ? w : 1;
+//	}
+//
+//	void PrintHero()const
+//	{
+//		SetConsoleTextAttribute(hConsole, ConsoleColors::GREEN);
+//		Print(symbol);
+//	}
+//	void ClearHero()const
+//	{
+//		Print(' ');
+//	}
+//	void MarkHero()const
+//	{
+//		SetConsoleTextAttribute(hConsole, ConsoleColors::RED);
+//		Print(symbol);
+//	}
+//	void ShowInfo()const
+//	{
+//		cout << "-------- Player [ " << name << " ] . HP : " << hp << " --------" << endl;
+//	}
+//	void SetPosition(int x, int y)
+//	{
+//		position.X = x >= 0 ? x : 0;
+//		position.Y = y >= 0 ? y : 0;
+//	}
+//	void Move()
+//	{
+//		ClearHero();
+//		bool isMoved = false;
+//		switch (dir)
+//		{
+//		case LEFT: isMoved = MoveLeft(); break;
+//		case RIGHT: isMoved = MoveRight(); break;
+//		case UP: isMoved = MoveUp(); break;
+//		case DOWN: isMoved = MoveDown();
+//		}
+//		isMoved ? PrintHero() : MarkHero();
+//	}
+//	void ChangeDirection(DIRECTION dir)
+//	{
+//		this->dir = dir;
+//	}
+//	bool IsValidPosition(int x, int y)const
+//	{
+//		return x >= 0 && y >= 0;
+//	}
+//	bool MoveRight()
+//	{
+//		if (IsValidPosition(position.X + 1, position.Y))
+//		{
+//			position.X++;
+//			return true;
+//		}
+//		return false;
+//	}
+//	bool MoveLeft()
+//	{
+//		if (IsValidPosition(position.X - 1, position.Y))
+//		{
+//			position.X--;
+//			return true;
+//		}
+//		return false;
+//	}
+//	bool MoveUp()
+//	{
+//		if (IsValidPosition(position.X, position.Y - 1))
+//		{
+//			position.Y--;
+//			return true;
+//		}
+//		return false;
+//	}
+//	bool MoveDown()
+//	{
+//		if (IsValidPosition(position.X, position.Y + 1))
+//		{
+//			position.Y++;
+//			return true;
+//		}
+//		return false;
+//	}
+//};
 
 
 
