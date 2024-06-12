000fe4acfc3533c6e5b7308a478c01e13407bb9e
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-12 06:56:15 -0700

    Kursova

--- CPlusPlus/2024-06-05 (Kursova)/NovaPoshta.cpp
+++ CPlusPlus/2024-06-05 (Kursova)/NovaPoshta.cpp
@@ -29,16 +29,17 @@ struct Ticket
 	string ticket_number;
 	Date date_mail_sent;
 	string order_address;
+	string service;
 	int mail_weight;
 	int cost_of_service;
 	bool isDelivered;
 	void Show()const
 	{
-		cout << "======== Ticket : " << ticket_number << " ========" << endl;
-		cout << "Date mail sent : " << date_mail_sent << endl;
-		cout << "Order address : " << order_address << endl;
-		cout << "Mail weight : " << mail_weight << endl;
-		cout << "Cost of service : " << cost_of_service << endl;
+		cout << "\t======== Ticket : " << ticket_number << " ========" << endl;
+		cout << "\tDate mail sent : " << date_mail_sent << endl;
+		cout << "\tOrder address : " << order_address << endl;
+		cout << "\tMail weight : " << mail_weight << endl;
+		cout << "\tCost of service : " << cost_of_service << endl;
 	}
 };
 
@@ -50,16 +51,16 @@ class Customer
 public:
 	Customer() : name("no name"), surname("no surname") {}
 
-	void AddTicket(string ticket_number, Date date_mail_sent, string sender_address, int mail_weight, int cost_of_service)
+	void AddTicket(string ticket_number, Date date_mail_sent, string sender_address, string service, int mail_weight, int cost_of_service)
 	{
-		ticket_of_mail.push_back(Ticket{ ticket_number, date_mail_sent, sender_address, mail_weight, cost_of_service, false });
+		ticket_of_mail.push_back(Ticket{ ticket_number, date_mail_sent, sender_address, service, mail_weight, cost_of_service, false });
 	}
 	void ShowTickets()const
 	{
 		system("cls");
 		if (!ticket_of_mail.empty())
 		{
-			for (auto ticket : ticket_of_mail)
+			for (Ticket ticket : ticket_of_mail)
 			{
 				ticket.Show();
 				cout << endl;
@@ -73,40 +74,103 @@ public:
 		cout << "Press any key to continue : ";
 		_getch();
 	}
-
-	~Customer()
-	{
-		delete[] ticket_of_mail;
-	}
 };
 
 class NovaPoshta
 {
 	string department_name;
 	list<string> services;
-	vector<string, list<Customer>> database_of_ordered_services;
-	vector<string, list<Customer>> database_of_performed_services;
+	vector<list<Customer>> database_of_ordered_services;
+	vector<list<Customer>> database_of_performed_services;
 public:
 	NovaPoshta() = delete;
-	NovaPoshta(string department_name) : department_name(department_name) {}
+	NovaPoshta(string department_name) : department_name(department_name)
+	{
+		services.push_back("Courier delivery");
+	}
 
 	void ShowServices()const
 	{
-
+		system("cls");
+		if (!services.empty())
+		{
+			for (string service : services)
+			{
+				cout << " - " << service << endl;
+			}
+		}
+		else
+		{
+			cout << "Not found services" << endl;
+		}
+		cout << endl;
+		cout << "Press any key to continue : ";
+		_getch();
 	}
 	void ChangeService()
 	{
-
+		system("cls");
+		if (!services.empty())
+		{
+			bool isValidData = true;
+			int choice;
+			int i;
+			while (choice < 1 || choice > services.size())
+			{
+				i = 0;
+				for (string service : services)
+				{
+					cout << i << " - " << service << endl;
+				}
+				cout << endl;
+				if (!isValidData)
+				{
+					cout << "Error choice! Try again." << endl;
+					cout << endl;
+				}
+				else isValidData = false;
+				cout << "Choice a service to change : ";
+				cin >> choice;
+			}
+			system("cls");
+			string service_;
+			cout << "Enter new service to change : ";
+			cin >> service_;
+			int i = 0;
+			for (string& service : services)
+			{
+				if (++i == choice)
+				{
+					service = service_;
+				}
+			}
+			cout << endl;
+			cout << "The service has been successfully changed" << endl;
+			cout << endl;
+			cout << "Press any key to continue : ";
+			_getch();
+		}
+		else
+		{
+			cout << "Not found services" << endl;
+			cout << endl;
+			cout << "Press any key to continue : ";
+			_getch();
+		}
 	}
 	void AddService()
 	{
-
+		
 	}
 	void DeleteService()
 	{
 
 	}
 	void AddOrder()
+	{
+
+	}
+	void DeliverTheOrder()
 	{
 
 	}
---
+++ CPlusPlus/2024-06-05 (Kursova)/Nova_poshta.cpp
@@ -0,0 +1,1 @@
+#include "Nova_poshta.h"
