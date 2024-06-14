5f3bad8224dae316e51e9c8e4fba67f79f3bea38
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-13 23:21:48 -0700

    Kursova_NovaPoshta

--- CPlusPlus/2024-06-05 (Kursova)/NovaPoshta.cpp
+++ CPlusPlus/2024-06-05 (Kursova)/NovaPoshta.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <conio.h>
 #include <vector>
 #include <list>
-#include <conio.h>
 using namespace std;
 
 struct Time
@@ -60,15 +60,21 @@ public:
 	{
 		return ticket_of_mail.size() == 0;
 	}
+	void PopTicket(int index)
+	{
+		auto it = ticket_of_mail.begin();
+		advance(it, index);
+		ticket_of_mail.erase(it);
+	}
 	Ticket& GetTicket(int index)
 	{
 		auto it = ticket_of_mail.begin();
 		advance(it, index);
 		return (*it);
 	}
-	void AddTicket(string ticket_number, Date date_mail_sent, string sender_address, string service, int mail_weight, int cost_of_service)
+	void AddTicket(string ticket_number, Date date_mail_sent, string sender_address, string service, int mail_weight, int cost_of_service, bool isDelivered = false)
 	{
-		ticket_of_mail.push_back(Ticket{ ticket_number, date_mail_sent, sender_address, service, mail_weight, cost_of_service, true });
+		ticket_of_mail.push_back(Ticket{ ticket_number, date_mail_sent, sender_address, service, mail_weight, cost_of_service, isDelivered });
 	}
 	bool ShowTickets()const
 	{
@@ -130,14 +136,14 @@ public:
 		if (!services.empty())
 		{
 			bool isValidData = true;
-			int choice;
+			int choice = 0;
 			int i;
 			while (choice < 1 || choice > services.size())
 			{
 				i = 0;
 				for (string service : services)
 				{
-					cout << i << " - " << service << endl;
+					cout << ++i << " - " << service << endl;
 				}
 				cout << endl;
 				if (!isValidData)
@@ -153,7 +159,7 @@ public:
 			string service_;
 			cout << "Enter new service to change : ";
 			cin >> service_;
-			int i = 0;
+			i = 0;
 			for (string& service : services)
 			{
 				if (++i == choice)
@@ -177,8 +183,6 @@ public:
 		cin >> newService;
 		services.push_back(newService);
 		cout << endl;
-		cout << "Service added" << endl;
-		cout << endl;
 		cout << "The service has been successfully added" << endl;
 		cout << endl;
 		cout << "Press any key to continue : ";
@@ -190,14 +194,14 @@ public:
 		if (!services.empty())
 		{
 			bool isValidData = true;
-			int choice;
+			int choice = 0;
 			int i;
 			while (choice < 1 || choice > services.size())
 			{
 				i = 0;
 				for (string service : services)
 				{
-					cout << i << " - " << service << endl;
+					cout << ++i << " - " << service << endl;
 				}
 				cout << endl;
 				if (!isValidData)
@@ -330,7 +334,16 @@ public:
 	}
 	void DeliverTheOrder()
 	{
-		if (!database_of_ordered_services.empty())
+		bool isFoundTickets = false;
+		for (Client client : database_of_ordered_services)
+		{
+			if (!client.empty())
+			{
+				isFoundTickets = true;
+				break;
+			}
+		}
+		if (!database_of_ordered_services.empty() && isFoundTickets)
 		{
 			int i;
 			int choice = 0;
@@ -390,15 +403,17 @@ public:
 				cin >> choice;
 			}
 			Ticket& ticket = (*it).GetTicket(choice - 1);
-
-
+			(*it_).AddTicket(ticket.ticket_number, ticket.date_mail_sent, ticket.order_address, ticket.service, ticket.mail_weight, ticket.cost_of_service, true);
+			(*it).PopTicket(choice - 1);
+			system("cls");
+			cout << "The order has been delivered" << endl;
 		}
 		else
 		{
 			system("cls");
 			cout << "No tickets found for undelivered orders!" << endl;
-			cout << endl;
 		}
+		cout << endl;
 		cout << "Press any key to continue : ";
 		_getch();
 	}
@@ -476,7 +491,61 @@ public:
 
 
 
+int Menu()
+{
+	int choice = 0;
+	bool isValidData = true;
+	while (choice < 1 || choice > 13)
+	{
+		system("cls");
+		cout << " 1 - Show services" << endl;
+		cout << " 2 - Change service" << endl;
+		cout << " 3 - Add service" << endl;
+		cout << " 4 - Delete service" << endl;
+		cout << " 5 - Add client to batabase" << endl;
+		cout << " 6 - Show clients" << endl;
+		cout << " 7 - Add ticket" << endl;
+		cout << " 8 - Deliver the order" << endl;
+		cout << " 9 - Save ordered services to file" << endl;
+		cout << "10 - Save performed services to file" << endl;
+		cout << "11 - Load ordered services from file" << endl;
+		cout << "12 - Load performed services from file" << endl;
+		cout << "13 - Exit" << endl;
+		if (!isValidData)
+		{
+			cout << "\nError choice! Try again.\n";
+		}
+		else isValidData = false;
+		cout << endl;
+		cout << "Enter your choice : ";
+		cin >> choice;
+	}
+	return choice;
+}
+enum MENU { SHOW_SERVICES = 1, CHANGE_SERVICE, ADD_SERVICE, DELETE_SERVICE, ADD_CLIENT, SHOW_CLIENTS, ADD_TICKET, DELIVER_THE_ORDER, SAVE_ORDERED_SERVICES, SAVE_PERFORMED_SERVICES, LOAD_ORDERED_SERVICES, LOAD_PERFORMED_SERVICES, EXIT };
 int main()
 {
-
+	NovaPoshta nova_poshta("Nova Poshta");
+	bool isExit = false;
+	while (!isExit)
+	{
+		switch (Menu())
+		{
+		case SHOW_SERVICES: nova_poshta.ShowServices(); break;
+		case CHANGE_SERVICE: nova_poshta.ChangeService(); break;
+		case ADD_SERVICE: nova_poshta.AddService(); break;
+		case DELETE_SERVICE: nova_poshta.DeleteService(); break;
+		case ADD_CLIENT: nova_poshta.AddClient(); break;
+		case SHOW_CLIENTS: nova_poshta.ShowClients(); break;
+		case ADD_TICKET: nova_poshta.AddTicket(); break;
+		case DELIVER_THE_ORDER: nova_poshta.DeliverTheOrder(); break;
+		case SAVE_ORDERED_SERVICES: nova_poshta.SaveDataBaseOfOrderedServicesToFile(); break;
+		case SAVE_PERFORMED_SERVICES: nova_poshta.SaveDataBaseOfPerformedServicesToFile(); break;
+		case LOAD_ORDERED_SERVICES: nova_poshta.LoadDataBaseOfOrderedServicesFromFile(); break;
+		case LOAD_PERFORMED_SERVICES: nova_poshta.LoadDataBaseOfPerformedServicesFromFile(); break;
+		case EXIT: isExit = true;
+		}
+	}
+	system("cls");
+	cout << "Good bye." << endl;
 }
